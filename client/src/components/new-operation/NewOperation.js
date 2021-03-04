1996270066581197e110ec0b28cc74c08318dad8
Nahuel <cristiannahuel95@gmail.com>
2021-03-04 20:59:40 -0300

    advance in the view of NewOperation

--- client/src/components/new-operation/NewOperation.js
+++ client/src/components/new-operation/NewOperation.js
@@ -2,6 +2,36 @@ import "./NewOperation.css"
 
 export const NewOperation = () => {
     return (
-        <div>NewOperation</div>
+        <div>
+            <div className="newOperation-container">
+                <div className="title">Create a new operation</div>
+                <div className="form-container">
+                    <div className="form-input">
+                        <label htmlFor="concept">Write a concept:</label>
+                        <input name="concept" type="text" placeholder="Concept"></input>
+                    </div>
+                    <div className="form-input">
+                        <label htmlFor="amount">Type an amount:</label>
+                        <input name="amount" type="number"></input>
+                    </div>
+                    <div className="form-input">
+                        <label htmlFor="date">Choose a date:</label>
+                        <input name="date" type="date"></input>
+                    </div>
+                    <div className="form-input">
+                        <label htmlFor="type">Select a type:</label>
+                        <select style={{ width: "11rem" }} name="type">
+                            <option>Income</option>
+                            <option>Out</option>
+                        </select>
+                    </div>
+                    <button style={{ gridColumn: "1/-1" }}>CLICK</button>
+                </div>
+            </div>
+            <div className="lists-container">
+                <div className="list-income">Income List</div>
+                <div className="list-out">Out list</div>
+            </div>
+        </div >
     )
 }
