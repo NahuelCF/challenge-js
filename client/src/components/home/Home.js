68a08c8b0c78ec7e5892df72c2f0e80b7f6b161d
Nahuel <cristiannahuel95@gmail.com>
2021-03-04 20:58:45 -0300

    redirect to NewOperation view

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,14 +1,14 @@
 import { HistoryCard } from "../history-card/HistoryCard"
 import "./Home.css"
 
-export const Home = () => {
+export const Home = (props) => {
     return (
         <div className="home-container">
             <div className="balance-container">
                 <h2>Balance</h2>
                 <div className="balance">0</div>
                 <h1>
-                    <button>Go to ABM</button>
+                    <button onClick={() => props.history.push("/new-operation")}>Go to ABM</button>
                 </h1>
             </div>
             <div className="history-container">
