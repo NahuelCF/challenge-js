ddd2df4694fae66b33f3c406c7b26f527025e23d
Nahuel <cristiannahuel95@gmail.com>
2021-03-05 15:37:06 -0300

    modifying HistoryCard for reuse it on the lists of NewOperation view

--- client/src/components/history-card/HistoryCard.js
+++ client/src/components/history-card/HistoryCard.js
@@ -1,11 +1,24 @@
 import "./HistoryCard.css"
 
-export const HistoryCard = () => {
+export const HistoryCard = (props) => {
+
+    const buttonsTemplate = () => {
+        if (props.addButtons) {
+            return (
+                <div>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+            )
+        }
+    }
+
     return (
         <div className="history-card">
             <div>$1500</div>
             <div>Income</div>
             <div>15/05/2020</div>
+            {buttonsTemplate()}
         </div>
     )
 }
