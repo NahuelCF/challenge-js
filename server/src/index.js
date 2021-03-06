86ae806eacede6747fe1946696000d995f7de4e5
Nahuel <cristiannahuel95@gmail.com>
2021-03-06 18:14:29 -0300

    configuring cors

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,13 @@ const app = express()
 
 // CONFIG
 app.set("json spaces", 2)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+})
 
 // MIDDLEWARES
 //Permite al server recibir json y entenderlo
