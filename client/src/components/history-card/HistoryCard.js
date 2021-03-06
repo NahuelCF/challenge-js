b40b53a0fa6a2589fd6582f627b08fa599ef394a
Nahuel <cristiannahuel95@gmail.com>
2021-03-06 16:14:48 -0300

    Adding the edit section that popup from the HistoryCard

--- client/src/components/history-card/HistoryCard.js
+++ client/src/components/history-card/HistoryCard.js
@@ -2,14 +2,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 import "./HistoryCard.css"
+import { useState } from 'react'
 
 export const HistoryCard = (props) => {
+
+    const [display, setDisplay] = useState("none")
 
     const buttonsTemplate = () => {
         if (props.addButtons) {
             return (
                 <div>
-                    <button className="card-button">
+                    <button onClick={() => changeDisplay()} className="card-button">
                         <FontAwesomeIcon icon={faEdit} />
                     </button>
                     <button className="card-button">
@@ -20,12 +23,46 @@ export const HistoryCard = (props) => {
         }
     }
 
+
+    const changeDisplay = () => {
+        if (display == "none") {
+            setDisplay("flex")
+        } else {
+            setDisplay("none")
+        }
+    }
+
     return (
-        <div className="history-card">
-            <div>$1500</div>
-            <div>Income</div>
-            <div>15/05/2020</div>
-            {buttonsTemplate()}
+        <div className="card-container">
+            <div className="history-card">
+                <div>$1500</div>
+                <div>Income</div>
+                <div>15/05/2020</div>
+                {buttonsTemplate()}
+            </div>
+
+            <div className="edit-container" style={{ display: display }}>
+                <div>
+                    <div className="form-input">
+                        <label htmlFor="concept">Write a concept:</label>
+                        <input name="concept" type="text" placeholder="Concept"></input>
+                    </div>
+                    <div className="form-input">
+                        <label htmlFor="amount">Type an amount:</label>
+                        <input name="amount" type="number"></input>
+                    </div>
+                </div>
+                <div>
+                    <div className="form-input">
+                        <label htmlFor="date">Choose a date:</label>
+                        <input name="date" type="date"></input>
+                    </div>
+                    <div className="card-buttons-edit">
+                        <button>Cancel</button>
+                        <button>Save</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
