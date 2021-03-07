6b960090951f6cf4fbb7a00d20d9795cdd265602
Nahuel <cristiannahuel95@gmail.com>
2021-03-07 12:55:39 -0300

    function to get the balance from the database

--- client/src/service.js
+++ client/src/service.js
@@ -1,11 +1,21 @@
 import { Operation } from "./domain/operation"
 
+const SERVER_URL = "http://localhost:3001"
+
 export const getOperations = async () => {
     let operations = []
-    await fetch("http://localhost:3001/getOperations")
+    await fetch(SERVER_URL + "/getOperations")
         .then(result => result.json())
         .then((items) => {
             operations = items.map(operation => Operation.fromJSON(operation))
         })
     return operations
+}
+
+export const getBalance = async () => {
+    let balance
+    await fetch(SERVER_URL + "/getBalance")
+        .then(result => result.json())
+        .then(item => balance = item)
+    return balance
 }
