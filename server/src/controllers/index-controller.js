9fa5518a8c7efebe4c8210698f7be2acc0dcd0b1
Nahuel <cristiannahuel95@gmail.com>
2021-03-07 18:54:33 -0300

    endpoint to update an operation done

--- server/src/controllers/index-controller.js
+++ server/src/controllers/index-controller.js
@@ -41,12 +41,21 @@ const createOperation = async (req, res) => {
         const response = await pool.query(`
             INSERT INTO operations (concept,amount,operationtype,operationdate) VALUES ($1,$2,$3,$4)
         `, [concept, amount, type, date])
-        res.json({
-            message: "Operation added successfully",
-            body: {
-                operation: { concept, amount, type, date }
-            }
-        })
+        res.status(200).json("Operation added successfully")
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+const updateOperation = async (req, res) => {
+    try {
+        const { concept, amount, type, date, id } = req.body
+        const response = await pool.query(`
+        UPDATE operations SET 
+        concept = $1, amount = $2, operationtype = $3, operationdate = $4 
+        WHERE id = $5;
+    `, [concept, amount, type, date, id])
+        res.status(200).json("Operation updated successfully")
     } catch (error) {
         res.status(500).json(error)
     }
@@ -55,5 +64,6 @@ const createOperation = async (req, res) => {
 module.exports = {
     getOperations,
     getBalance,
-    createOperation
+    createOperation,
+    updateOperation
 }
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,4 +1,4 @@
-const { getOperations, getBalance, createOperation } = require('../controllers/index-controller')
+const { getOperations, getBalance, createOperation, updateOperation } = require('../controllers/index-controller')
 
 // Permite definir rutas para el servidor
 const { Router } = require("express")
@@ -9,5 +9,6 @@ const operations = require("../sample.json")
 router.get("/getOperations", getOperations)
 router.get("/getBalance", getBalance)
 router.post("/createOperation", createOperation)
+router.put("/updateOperation", updateOperation)
 
 module.exports = router
