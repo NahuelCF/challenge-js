685cf67866ff46ffeea5b7ce3ab4835446cb454e
Nahuel <cristiannahuel95@gmail.com>
2021-03-07 12:55:13 -0300

    Home now show the balance received from the server

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,25 +2,28 @@ import { HistoryCard } from "../history-card/HistoryCard"
 import "./Home.css"
 
 import { useState, useEffect } from "react"
-import { getOperations } from "../../service"
+import { getBalance, getOperations } from "../../service"
 
 export const Home = (props) => {
 
     const [operations, setOperations] = useState([])
+    const [balance, setBalance] = useState("")
 
     useEffect(() => {
-        const getOperationsLocal = async () => {
+        const init = async () => {
             let operations = await getOperations()
             setOperations(operations.slice(1).slice(-10))
+            let balance = await getBalance()
+            setBalance(balance)
         }
-        getOperationsLocal()
+        init()
     }, [])
 
     return (
         <div className="home-container">
             <div className="balance-container">
                 <h2>Balance</h2>
-                <div className="balance">0</div>
+                <div className="balance">${balance}</div>
                 <h1>
                     <button onClick={() => props.history.push("/new-operation")}>Go to ABM</button>
                 </h1>
