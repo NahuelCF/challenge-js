ccbd5e45b6197614a2c084dd62f2ab0a8d909b54
Nahuel <cristiannahuel95@gmail.com>
2021-03-07 15:22:47 -0300

    fix - now id is serial

--- database.sql
+++ database.sql
@@ -1,23 +1,23 @@
 CREATE DATABASE challenge;
 
 CREATE TABLE operations(
-    id SERIAL NOT NULL,
+    id SERIAL PRIMARY KEY,
     concept TEXT,
     amount INTEGER,
     operationType TEXT,
     operationDate DATE
 );
 
-INSERT INTO operations VALUES
-(1,'Example', 2000, 'income', '2021-01-15'),
-(2,'Example', 1000, 'out', '2021-02-15'),
-(3,'Example', 2000, 'income', '2021-02-05'),
-(4,'Example', 3300, 'out', '2021-02-11'),
-(5,'Example', 2500, 'income', '2021-02-01'),
-(6,'Example', 1500, 'out', '2021-02-22'),
-(7,'Example', 1300, 'income', '2021-03-05'),
-(8,'Example', 2100, 'out', '2021-03-01'),
-(9,'Example', 4200, 'income', '2021-02-12'),
-(10,'Example', 900, 'out', '2021-02-23'),
-(11,'Example', 2400, 'income', '2021-03-06'),
-(12,'Example', 1900, 'out', '2021-03-06');
+INSERT INTO operations (concept,amount,operationtype,operationdate) VALUES 
+('Example', 2000, 'income', '2021-01-15'),
+('Example', 1000, 'out', '2021-02-15'),
+('Example', 2000, 'income', '2021-02-05'),
+('Example', 3300, 'out', '2021-02-11'),
+('Example', 2500, 'income', '2021-02-01'),
+('Example', 1500, 'out', '2021-02-22'),
+('Example', 1300, 'income', '2021-03-05'),
+('Example', 2100, 'out', '2021-03-01'),
+('Example', 4200, 'income', '2021-02-12'),
+('Example', 900, 'out', '2021-02-23'),
+('Example', 2400, 'income', '2021-03-06'),
+('Example', 1900, 'out', '2021-03-06');
